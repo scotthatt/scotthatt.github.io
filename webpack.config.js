975a08cd466e950d6aaa97e150e0afd8edb187e3
Kelly J Andrews <kelly@kellyjandrews.com>
2016-10-24 07:50:01 -0400

    adding dev node_env to reduce calls to cdn

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,12 @@ module.exports = {
     extensions: ['', '.js', '.jsx', '.json']
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': '"'+process.env.NODE_ENV+'"' || '""'
+      }
+    })
   ],
   module: {
     loaders: [
