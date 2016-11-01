2e936af473cace6ae253a14b24b4605d9f638621
Kelly J Andrews <kelly@kellyjandrews.com>
2016-11-01 04:30:29 -0400

    updates to mobile rendering, building image detail

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router';
 import Image from './Image';
 import images from '../data/images';
+
+function orderImages(a, b) {
+  if (a.order < b.order) {
+    return -1;
+  }
+  if (a.order > b.order) {
+    return 1;
+  }
+  return 0;
+}
 
 const ImageGrid = () => (
   <section>
     {
-      images.map(image => (
-        <Image image={ image.image } />
+      images.sort(orderImages).map(image => (
+        <Link><Image image={ image.image } /></Link>
       ))
     }
   </section>
